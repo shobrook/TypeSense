c0656177c63372e0fd2c8b0521526fe6f97717c5
alichtman <aaronlichtman@gmail.com>
2018-03-03 07:39:18 -0600

    Tried adding require js on the client side

--- extension/background.js
+++ extension/background.js
@@ -1,5 +1,25 @@
-var bcrypt = require('libraries/bcrypt.js');
-var crypto = require('crypto');
+// FIXME: http://requirejs.org/docs/api.html#jsfiles
+
+var requirejs = 'libraries/require.js';
+
+requirejs.config({
+    //Pass the top-level main.js/index.js require
+    //function to requirejs so that node modules
+    //are loaded relative to the top-level JS file.
+    nodeRequire: require
+});
+
+requirejs(['bcrypt', 'crypto'], function   (foo,   bar) {
+    //foo and bar are loaded according to requirejs
+    //config, but if not found, then node's require
+    //is used to load the module.
+});
+
+// var bcrypt = require('libraries/bcrypt.js');
+// var crypto = require('crypto');
+var bcrypt = requirejs('bcrypt')
+var crypto = requirejs('crypto')
+
 
 /* Globals */
 
