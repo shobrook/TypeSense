5d6546e853b7cbcf99123729a80f39d05bc76b45
Jonathan Shobrook <shobrookj@gmail.com>
2018-03-08 01:30:35 -0600

    Added new message listener

--- extension/content/listeners.js
+++ extension/content/listeners.js
@@ -1,14 +1,11 @@
 /* Globals */
 
-var listenerPort = chrome.runtime.connect(window.localStorage.getItem('typesense-id'), {name: "listener"});
 
-
-/* Main */
-
-
-var eventListeners = function(getFBID) {
-	function getUserID() {
-	  var messageList = document.querySelectorAll("[class='_1t_p clearfix']");
+const listenerPort = chrome.runtime.connect(window.localStorage.getItem('typesense-id'), {name: "listener"});
+const eventListeners = function(getFBID) {
+	// Pulls the current user's Facebook ID
+	const getUserID = () => {
+	  let messageList = document.querySelectorAll("[class='_1t_p clearfix']");
 		messageList.forEach(function(messageNode) {
 			(messageNode.getElementsByClassName("_41ud")).forEach(function(message) {
 				if (message) {
@@ -18,9 +15,11 @@ var eventListeners = function(getFBID) {
 				}
 			});
 		});
+	}
 
-	function getRecipientID() {
-	  var messageList = document.querySelectorAll("[class='_1t_p clearfix']");
+	// Pulls the current recipient's Facebook ID
+	const getRecipientID = () => {
+	  let messageList = document.querySelectorAll("[class='_1t_p clearfix']");
 		messageList.forEach(function(messageNode) {
 			(messageNode.getElementsByClassName("_41ud")).forEach(function(message) {
 				if (message) {
@@ -30,67 +29,33 @@ var eventListeners = function(getFBID) {
 		});
 	}
 
-	/*
-	function getElementByAttribute(attr, value, root) {
-	    root = root || document.body;
-	    if(root.hasAttribute(attr) && root.getAttribute(attr) == value) {
-	        return root;
-	    }
-	    var children = root.children,
-	        element;
-	    for(var i = children.length; i--; ) {
-	        element = getElementByAttribute(attr, value, children[i]);
-	        if(element) {
-	            return element;
-	        }
-	    }
-	    return null;
-	}
+	// Scrapes the last 23 messages in the current conversation
+	const scrapeMessages = () => {
+		let scrapedMessages = [];
 
-	window.onload = function () {
-	  console.log("getUserId: " + getUserId());
-	  console.log("getRecipientId: " + getRecipientId());
-	  injectButton();
-	  document.getElementById('js_1').addEventListener('DOMNodeInserted', function(event) {
-	    if(event.target.parentNode.id == 'js_1') {
-	      console.log("sdsa");
-	      POST("http://localhost:5000/TypeSense/api/new_connection", {email: "shobrookj@gmail.com", fb_id: getRecipientId(), messages: []}, function(responseData) {
-	        chrome.extension.onConnect.addListener(function(port) {
-	              console.log("Connected .....");
-	              port.postMessage(responseData);
-	         });
-	      });
-	    }
-	  }, false);
-	};
-	*/
+		// TODO: Only scrape 23 messages
 
-	// TODO: Add event listener for new messages
-	// TODO: Add event listener for conversation change
-
-	window.postMessage({type: "event-notifications", value: {"fb_id": getRecipientID(), "messages": function() {
-		var scrapedMessages = [];
-
-		var containerNode = document.getElementsByClassName('__i_')[0];
+		let containerNode = document.getElementsByClassName('__i_')[0];
 		containerNode.childNodes.forEach(function(child) {
 			if (child.tagName == 'DIV' && child.id.length > 0) {
 				child.childNodes.forEach(function(c) {
 					if (c.tagName == 'DIV') {
-						var msgWrapperNodes = c.childNodes[0].getElementsByClassName('clearfix');
+						let msgWrapperNodes = c.childNodes[0].getElementsByClassName('clearfix');
 
-						for (var i = 0; i < msgWrapperNodes.length; i++) {
-							var msgNode = msgWrapperNodes[i].childNodes[0].childNodes[0];
+						for (let i = 0; i < msgWrapperNodes.length; i++) {
+							let msgNode = msgWrapperNodes[i].childNodes[0].childNodes[0];
 
-							if (msgNode == undefined || msgNode == null) continue; // Detects if message is rich media content
+							// Passes if message has rich media content
+							if (msgNode == undefined || msgNode == null)
+								continue;
 
 							// TODO: Check for emojis
 
-							var author = true;
+							let author = true;
 							if (window.getComputedStyle(msgWrapperNodes[i].childNodes[0], null).getPropertyValue("background-color") == "rgb(241, 240, 240)")
 								author = false;
-							var position = msgNode.getBoundingClientRect();
 
-							scrapedMessages.push({"author": author, "message": msgNode.textContent, "coordinates": [position.left + window.pageXOffset, position.top + window.pageYOffset]});
+							scrapedMessages.push({"author": author, "message": msgNode.textContent});
 						}
 					}
 				});
@@ -99,15 +64,28 @@ var eventListeners = function(getFBID) {
 
 		console.log("Scraped all loaded messages.");
 		return scrapedMessages;
-	}}}, '*'); // TODO: Run this whenever an event is detected
+	}
+
+	// Listens for a new message
+	document.getElementById("js_1").addEventListener('DOMNodeInserted', function(event) {
+    if (event.target.parentNode.id == "js_1") {
+			window.postMessage({type: "event-notifications", value: {"fb_id": getRecipientID(), "messages": scrapeMessages()}}, '*');
+    }
+	}, false);
+
+	// TODO: Add event listener for conversation change
 }
 
 // Prepares the JS injection
-var listenerInject = function() {
+const listenerInject = function() {
 	var script = document.createElement("script");
 	script.textContent = "(" + eventListeners.toString() + ")();";
 	document.head.appendChild(script);
 }
+
+
+/* Main */
+
 
 // Listens for the "inject-listeners" event from the background script
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
