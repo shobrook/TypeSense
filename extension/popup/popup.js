8645a42161c71eaacabc5d79209e547967e4d775
Jonathan Shobrook <shobrookj@gmail.com>
2018-05-14 23:15:06 -0400

    Refactored and cleaned up createGraph()

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,91 +1,86 @@
 const createGraph = (data) => {
-	//var data = data.reverse();
+	const sentimentTable = data.reverse();
 
-	var margin = {top: 0, right: 0, bottom: 0, left: 0},
-	width = 400;
-	height = 320;
+	let margin = {"top": 0, "right": 0, "bottom": 0, "left": 0}
+	let width = 400;
+	let height = 320;
 
-	// Add svg to
-	var svg = d3.select('#graph')
-				.append('svg')
-				.attr('width', width + margin.left + margin.right)
-				.attr('height', height + margin.top + margin.bottom)
-				.append('g')
-				.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+	var graphSVG = d3.select("#graph")
+							.append("svg")
+							.attr("width", width + margin["left"] + margin["right"])
+							.attr("height", height + margin["top"] + margin["bottom"])
+							.append('g')
+							.attr("transform", "translate(" + margin["left"] + ',' + margin["top"] + ')');
 
-	// set the ranges
-	var y = d3.scaleBand().range([height, 0]).padding(0.52);
-
+	// Sets the ranges
+	var y = d3.scaleBand().range([height, 0]).padding(0); // 0.52
 	var x = d3.scaleLinear().range([0, width]);
 
 	// Scale the range of the data in the domains
-	x.domain(d3.extent(data, function (d) {
-	return d.sentiment;
+	x.domain(d3.extent(sentimentTable, (table) => {
+		return table["sentiment"];
 	}));
-	y.domain(data.map(function (d) {
-	return d.id;
+	y.domain(sentimentTable.map((table) => {
+		return table["id"];
 	}));
 
-	// append the rectangles for the bar chart
-	svg.selectAll(".bar")
-	   .data(data)
-	   .enter()
-	   .append("rect")
-	   .attr("class", function (d) {
-			return "bar bar--" + (d.sentiment < 0 ? "negative" : "positive");
-	})
-	.attr("x", function (d) {
-		return x(Math.min(7, d.sentiment * 0.5));
-	})
-	.attr("y", function (d) {
-		return y(d.id);
-	})
-	.attr("width", function (d) {
-		return Math.abs(x(d.sentiment * 0.55) - x(0));
-	})
-	.attr("fill", function(d) {
-		if (d.received) {
-			if (d.sentiment < 0) {
-				return "#ff5e66";
-			} else {
-				return "#6dd792";
-			}
-		} else {
-			return "rgba(0, 0, 0, 0.0)"
-		}
-	})
-	.attr("stroke-linecap", "round")
-	.attr("stroke", function(d) {
-		if (d.sentiment < 0) {
-			return "#ff5e66";
-		} else {
-			return "#6dd792";
-		}
-	})
-	.attr("stroke-width", "2px")
-	.attr("height", y.bandwidth())
-	.attr("rx", "1.5px");
+	// Appends the rectangles for the bar chart
+	graphSVG.selectAll(".bar")
+					.data(sentimentTable)
+					.enter()
+					.append("rect")
+					.attr("class", (table) => {
+						return "bar bar--" + (table["sentiment"] < 0 ? "negative" : "positive");
+					})
+					.attr('x', (table) => {
+						return x(Math.min(7, table["sentiment"] * 0.5));
+					})
+					.attr('y', (table) => {
+						return y(table["id"]);
+					})
+					.attr("width", (table) => {
+						return Math.abs(x(table["sentiment"] * 0.55) - x(0));
+					})
+					.attr("fill", (table) => {
+						if (table["received"]) {
+							if (table["sentiment"] < 0)
+								return "#FF5E66";
+							else
+								return "#6DD792";
+						} else {
+							return "RGBA(0, 0, 0, 0.0)";
+						}
+					})
+					.attr("stroke-linecap", "round")
+					.attr("stroke", (table) => {
+						if (table["sentiment"] < 0)
+							return "#FF5E66";
+						else
+							return "#6DD792";
+					})
+					.attr("stroke-width", "2px")
+					.attr("height", y.bandwidth())
+					.attr("rx", "1.5px");
 
-	svg.append("line")
-	   .attr("x1", (width / 2))
-	   .attr("y1", 0)
-	   .attr("x2", (width / 2))
-	   .attr("y2", height)
-	   .style("stroke-width", 2)
-	   .style("stroke", "#F1F0F0")
-	   .style("fill", "1");
+	graphSVG.append("line")
+					.attr("x1", width / 2)
+					.attr("y1", 0)
+					.attr("x2", width / 2)
+					.attr("y2", height)
+					.style("stroke-width", 2)
+					.style("stroke", "#F1F0F0")
+					.style("fill", '1');
 
-	svg.append("line")
-	   .attr("x1", 0)
-	   .attr("y1", height - 1)
-	   .attr("x2", width)
-	   .attr("y2", height - 1)
-	   .style("stroke-width", 2)
-	   .style("stroke", "#F1F0F0")
-	   .style("fill", "1");
+	graphSVG.append("line")
+					.attr("x1", 0)
+					.attr("y1", height - 1)
+					.attr("x2", width)
+					.attr("y2", height - 1)
+					.style("stroke-width", 2)
+					.style("stroke", "#F1F0F0")
+					.style("fill", '1');
 }
 
 chrome.storage.local.get("currentThread", (sentimentTable) => {
-	console.log(sentimentTable); // TEMP
-	createGraph(Array.from(sentimentTable));
+	createGraph(sentimentTable["currentThread"]);
 });
