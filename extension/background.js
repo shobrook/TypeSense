a02d32d33cc2fce8454bc9c567ea82122ec6c552
Jonathan Shobrook <shobrookj@gmail.com>
2018-05-18 02:05:17 -0400

    This is majorly fucked up but at least we have data flow

--- extension/background.js
+++ extension/background.js
@@ -9,11 +9,11 @@ const endpoint = "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?v
 const credentials = {"username": "21a9e424-69e0-4a8f-995c-19d1b5f9e72e", "password": "xctC0k8jpWZy"};
 
 // Creates a synchronous HTTP GET request
-const get = (url, credentials) => { // NOTE: Add `callback` parameter to make asynchronous
+const get = (url, credentials, callback) => {
   let xhr = new XMLHttpRequest();
   xhr.onreadystatechange = () => {
     if (xhr.readyState == 4 && xhr.status == 200) {
-      return xhr.responseText; // NOTE: Replace with `return callback(xhr.responseText);` to make asynchronous
+      callback(xhr.responseText);
     }
   }
 
@@ -44,12 +44,12 @@ const message = (content) => {
 	});
 }
 
-// Pos/neg signs for Watson tone categories
+// +/- signs for Watson tone categories
 const toneSigns = {
   "sadness": -1,
   "anger": -1,
   "fear": -1,
-  "tentative": 1,
+  "tentative": -1,
   "joy": 1,
   "analytical": 1,
   "confident": 1
@@ -57,77 +57,33 @@ const toneSigns = {
 
 // Transforms a messages object into a sentimentTable object
 const analyzeSentiment = (messages) => {
-  // Measures sentiment as a growing window
-  const methodOne = (messages) => {
-    let sentimentTable = [];
+  var sentimentTable = [];
 
-    let windows = Array.apply(null, Array(messages.length)).map((_, idx) => {
-      let slicedMessageObjs = messages.slice(0, idx + 1);
-      let messageWindows = slicedMessageObjs.map((messageObj) => { return messageObj["message"]; });
+  let windows = Array.apply(null, Array(messages.length)).map((_, idx) => {
+    let slicedMessageObjs = messages.slice(0, idx + 1);
+    let messageWindows = slicedMessageObjs.map((messageObj) => { return messageObj["message"]; });
 
-      return messageWindows;
-    });
-    for (var idx = 0; idx < messages.length; idx++) {
-      let payload = windows[idx].join(' ').replace(/ /g, "%20");
-      /*
-      // NOTE: This is what an asynchronous request *should* look like, but the callback
-      // function can't access the sentimentTable or messages variables for some reason.
+    return messageWindows;
+  });
+  for (let idx = 0; idx < messages.length; idx++) {
+    let payload = windows[idx].join(' ').replace(/ /g, "%20");
+    //let newMessages = messages; // TEMP
 
-      get(endpoint + payload, credentials, (response) => {
-        let tones = JSON.parse(response)["document_tone"]["tones"];
-        let sentimentScores = tones.map((tone) => { return toneSigns[tone["tone_id"]] * tone["score"]; });
-
-        sentimentTable.push({
-          "id": idx,
-          "message": messages[idx]["message"],
-          "received": messages[idx]["received"],
-          "sentiment": sentimentScores.reduce((sum, score) => { return sum + score; })
-        });
-      });
-      */
-
-      let tones = JSON.parse(get(endpoint + payload, credentials))["document_tone"]["tones"];
+    get(endpoint + payload, credentials, (response) => {
+      let tones = JSON.parse(response)["document_tone"]["tones"];
       let sentimentScores = tones.map((tone) => { return toneSigns[tone["tone_id"]] * tone["score"]; });
+      let newMessages = messages;
 
       sentimentTable.push({
         "id": idx,
-        "message": messages[idx]["message"],
-        "received": messages[idx]["received"],
-        "sentiment": sentimentScores.reduce((sum, score) => { return sum + score; })
+        "message": newMessages[idx]["message"],
+        "received": newMessages[idx]["received"],
+        "sentiment": sentimentScores.length > 0 ? Math.round(100 * (sentimentScores.reduce((sum, score) => { return sum + score; }, 0) / sentimentScores.length)) : 50
       });
-    }
-
-    return sentimentTable;
+    });
   }
 
-  let sentimentTable = methodOne(messages);
-  if (sentimentTable.length > 17) {
-    return sentimentTable.slice(sentimentTable.length - 17);
-  } else {
-    return sentimentTable;
-  }
-
-  /*
-	return [
-    {sentiment: -10, id: 0, received: true},
-    {sentiment: 40, id: 1, received: false},
-    {sentiment: -10, id: 2, received: true},
-    {sentiment: -50, id: 3, received: true},
-    {sentiment: 30, id: 4, received: false},
-    {sentiment: 60, id: 5, received: true},
-    {sentiment: 50, id: 6, received: true},
-    {sentiment: -20, id: 7, received: false},
-    {sentiment: -10, id: 8, received: true},
-    {sentiment: 40, id: 9, received: false},
-    {sentiment: -10, id: 10, received: true},
-    {sentiment: -50, id: 11, received: true},
-    {sentiment: 30, id: 12, received: false},
-    {sentiment: 60, id: 13, received: true},
-    {sentiment: 50, id: 14, received: true},
-    {sentiment: -20, id: 15, received: false},
-    {sentiment: -20, id: 16, received: false}
-  ]
-  */
+  return sentimentTable;
 }
 
 
@@ -136,7 +92,6 @@ const analyzeSentiment = (messages) => {
 
 // Listens for messenger.com to be loaded and tells listeners.js to inject the event handlers
 chrome.webNavigation.onCompleted.addListener((details) => {
-	// BUG: Message is sometimes sent before the DOM is fully loaded
 	if (details.url.includes("messenger.com")) {
 		message({"message": "injectListeners"});
 	}
@@ -158,21 +113,27 @@ chrome.runtime.onInstalled.addListener((details) => {
 chrome.runtime.onConnect.addListener((port) => {
 	port.onMessage.addListener((msg) => {
     if (port.name == "listener") { // Handles requests from listeners.js
-      // OPTIMIZE: Compare msg (scraped messages) w/ what's currently in local storage; find where the two sets stop intersecting,
-      // then analyze the remaining part of msg and append it to the local set, and slice to ensure it's at length 17
-
       let sentimentTable = analyzeSentiment(msg.messages);
 
-      storage.set({"currentThread": sentimentTable}, () => { // TODO: Memoize conversations
-        console.log("Populated conversation's sentiment table.");
-      });
+      // This is genuinely pathetic
+      setTimeout(() => {
+        sentimentTable.sort((m, n) => { return m["id"] - n["id"]; });
 
-      // Updates the browser action icon according to sentiment change
-      if (sentimentTable[sentimentTable.length - 1]["sentiment"] >= sentimentTable[sentimentTable.length - 2]["sentiment"]) { // Sentiment increased
-        chrome.browserAction.setIcon({path: "../assets/icon_green.png"});
-      } else { // Sentiment decreased
-        chrome.browserAction.setIcon({path: "../assets/icon_red.png"});
-      }
+        if (sentimentTable.length > 17) {
+          sentimentTable = sentimentTable.slice(sentimentTable.length - 17);
+        }
+
+        storage.set({"currentThread": sentimentTable}, () => { // TODO: Memoize conversations
+          console.log(sentimentTable);
+        });
+
+        // Updates the browser action icon according to sentiment change
+        if (sentimentTable[sentimentTable.length - 1]["sentiment"] >= sentimentTable[sentimentTable.length - 2]["sentiment"]) { // Sentiment increased
+          chrome.browserAction.setIcon({path: "../assets/icon_green.png"});
+        } else { // Sentiment decreased
+          chrome.browserAction.setIcon({path: "../assets/icon_red.png"});
+        }
+      }, 10000)
     }
   });
 });
